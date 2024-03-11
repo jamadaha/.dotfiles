5f4625c58362f21624bec8e1f09cb4831fddab42
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-03-11 17:15:31 +0100

    feat: nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,16 @@
+require("map_leader")
+require("plugins")
+require("options")
+require("keymaps")
+require("plugin_config")
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
---
+++ .config/nvim/lua/plugin_config/init.lua
@@ -0,0 +1,4 @@
+require("plugin_config.telescope")
+require("plugin_config.treesitter")
+require("plugin_config.lsp")
+require("plugin_config.cmp")
